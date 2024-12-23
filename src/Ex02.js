56da6588f5df414ed37347c38711dd4e77d5396b
hjlee-kr <sandjtech@naver.com>
2024-12-23 11:40:41 +0900

    state

--- src/Ex02.js
+++ src/Ex02.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';//state사용하기위한 선언
+
 function Header(props) {
     console.log(props);
     return (
@@ -9,19 +11,22 @@ function Header(props) {
                 // react는 return false; 동작이 없습니다.
                 // react에서 페이지 이동이 안되게 하려면
                 event.preventDefault();
-                alert("Hi");
+                //alert("Hi");
+                props.onChangeMode();
             }}>{props.title}</a></h1>
         </header>
     );
 }
 
 function Nav(props) {
+    console.log(props);
     const lis = [];
     for(let i=0 ; i<props.topics.length ; i++) {
         let t = props.topics[i];
-        lis.push(<li key={t.id}><a href={"/read/" + t.id} onClick={
+        lis.push(<li key={t.id}><a id={t.id} href={"/read/" + t.id} onClick={
             (event) => {
-                alert(t.id);
+                event.preventDefault();
+                props.onChangeMode(Number(event.target.id));
             }
         }>{t.title}</a></li>)
     }
@@ -35,6 +40,7 @@ function Nav(props) {
 }
 
 function Article(props) {
+    console.log("Article:",props);
     return (
         <article>
             <h2>{props.title}</h2>
@@ -44,19 +50,51 @@ function Article(props) {
 }
 
 function Ex02() {
+    //let mode = 'WELCOME';
+    // useState는 변수값이 변경되었을때 다시랜더링을 해라
+    //let _mode = useState('WELCOME');
+    //let mode = _mode[0];//변수
+    //let setMode = _mode[1];//setter
+    // 위의 세줄을 한줄로
+    let [mode, setMode] = useState('WELCOME');
+    let [id, setId] = useState(null);
+
     const topics = [
         {id:1, title:"html", content:"html is ..."},
         {id:2, title:"css", content:"css is ..."},
         {id:3, title:"javascript", content:"javascript is ..."},
         {id:4, title:"react", content:"react is ..."}
     ];
+    let element = null;
+    if (mode === 'WELCOME') {
+        element = <Article title="Welcome" content="Hello, React"/>;
+    } else if (mode === 'HI' && id !== null) {
+        let title, content = null;
+        for (let i=0; i<topics.length ; i++) {
+            //console.log(typeof topics[i].id, typeof id);
+            if (topics[i].id === id) {
+                //console.log("topics[i].id == id");
+                title = topics[i].title;
+                content = topics[i].content;
+                //console.log(title, content);
+                break;
+            }
+        }
+        console.log("title:", title, "content:", content);
+        element = <Article title={title} content={content} />;
+        console.log(content);
+    }
     return (
         <div>
             {/*이것은 주석입니다*/}
-            <Header title="React"/>
-            <Nav topics={topics}/>
-            <Article title="Welcome" content="Hello, React"/>
-            <Article title="Hi" content="Hello, World!!" />
+            <Header title="React" onChangeMode={()=>{
+                setMode('WELCOME');
+            }} />
+            <Nav topics={topics} onChangeMode={(_id)=>{
+                setMode('HI');
+                setId(_id);                
+            }} />
+            {element}
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import Ex02 from './Ex02';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/*<App />*/}
-    {/*<Hello />*/}
+//  <React.StrictMode>
     <Ex02 />
-  </React.StrictMode>
+//  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
